6dbdf1f7564f3df4b986ebdb956424ea5626d776
Foxtrot47 <37036622+Foxtrot47@users.noreply.github.com>
2020-10-20 18:41:56 +0530

    Startup

---
+++ rr_build.sh
@@ -0,0 +1,10 @@
+ sudo apt-get install git-core
+ git clone https://github.com/akhilnarang/scripts
+ cd scripts
+ git config --global user.name "Foxtrot47"
+ git config --global user.email "jjneutron@outlook.com"
+ bash setup/android_build_env.sh
+ 
+ 
+ 
+ 
