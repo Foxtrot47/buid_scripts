0f354de79d8bea9f4ad6e55e352d0339b4c653ae
Foxtrot47 <37036622+Foxtrot47@users.noreply.github.com>
2020-10-21 11:57:30 +0530

    Update rr_build.sh

--- rr_build.sh
+++ rr_build.sh
@@ -6,5 +6,23 @@
  bash setup/android_build_env.sh
  
  
+ cd /home/foxtrot47/rr
+ 
+ export TARGET_FACE_UNLOCK_SUPPORTED=true
+ export USE_CCACHE=1
+ export CCACHE_EXEC=/usr/bin/ccache
+ export KBUILD_BUILD_USER=Foxtrot47
+ export KBUILD_BUILD_HOST=arch_labs
+ export SELINUX_IGNORE_NEVERALLOW=true
+ source build/envsetup.sh
+ 
+ lunch rr_rolex-user
+ mka bacon -j8
+ 
+ cd $OUT
+ 
+ rclone copy RR*.md* drive:Rolex/ResurrectionRemix
+ 
+ 
  
  
